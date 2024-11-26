2101997887b5a8b3d5ca14337ec58a92a296ccdb
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-11-26 11:00:59 +0900

    [Add] UI매니저, View, Model, Controller 추가
    UI매니저와 MVC 패턴에 필요한 3요소 추가

---
+++ Assets/02.Scripts/05.UI/UIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UIController : MonoBehaviour
+{
+    protected UIView view;
+    protected UIModel model;
+
+    public virtual void Initialize(UIView view, UIModel model)
+    {
+        this.view = view;
+        this.model = model;
+
+        view.Initialize();
+    }
+
+    public abstract void OnShow();      // View가 활성화될 때 호출
+    public abstract void OnHide();      // View가 비활성화될 때 호출
+    public abstract void UpdateView();  // Model 변경 시 View 갱신
+}
