a3945f24c4d7a6b38e35e000683c77f73d563efc
SONGMYEONGSEONG <100818983+SONGMYEONGSEONG@users.noreply.github.com>
2024-11-26 15:50:15 +0900

    Chore : CSV Write 기능 구현 중

--- Assets/02.Scripts/00.Utils/CSVController.cs
+++ Assets/02.Scripts/00.Utils/CSVController.cs
@@ -1,18 +1,43 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using UnityEngine.Analytics;
 
-public enum CSVType
+public enum CSVEnumType
 {
-    
+    Bullet = 0,
+    Enemy = 1,
+
 }
 
-public class CSVReader
+public class CSVController
 {
     static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))"; //한 행에 대한 데이터 분리를 위한 세퍼레이트(분리자)
     static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";//가져온 CSV데이터를 엔터단위로 끊기위한 분리자
     static char[] TRIM_CHARS = { '\"' }; //
+
+    private StringBuilder strBuilder = new StringBuilder();
+
+    public void Write(string filePath, SaveData saveData)
+    {
+        TextWriter tw = new StreamWriter(filePath, false, Encoding.UTF8 );
+
+        tw.WriteLine(saveData.saveFileHeader);
+        tw.WriteLine(saveData.saveFileHeaderType);
+
+        strBuilder.Clear();
+        strBuilder.Append(saveData.UID);
+        strBuilder.Append(",");
+        strBuilder.Append(saveData.Name);
+        strBuilder.Append(",");
+        strBuilder.Append(saveData.Datas);
+        tw.WriteLine(strBuilder.ToString());
+        tw.Close();
+
+    }
 
     public List<Dictionary<string, object>> Read(string file)
     {
@@ -63,8 +88,34 @@ public class CSVReader
                 return bool.Parse(value);  //bool
             case "string":
                 return value;  //string
-            default:
-                throw new Exception($"지원하지 않는 자료형 타입입니다.: {type}");
+            //default:
+            //    throw new Exception($"지원하지 않는 자료형 타입입니다.: {type}");
         }
+
+        if(type.StartsWith("List<Enum"))
+        {
+
+        }
+        else if (type.StartsWith("List<"))
+        {
+            var itemType = type.Substring(5, type.Length - 6);
+
+            switch (itemType)
+            {
+                case "int":
+                    return value.Split(',').Select(int.Parse).ToList();  //List<int>
+                case "float":
+                    return value.Split(',').Select(float.Parse).ToList(); //List<float>
+                case "double":
+                    return value.Split(',').Select(double.Parse).ToList(); //List<double>
+                case "bool":
+                    return value.Split(',').Select(bool.Parse).ToList();  //List<bool>
+                case "string":
+                    return value.Split(',').Select(v => v.Trim()).ToList();  //List<string>
+                    
+            }
+        }
+
+        return null;
     }
 }
--- Assets/02.Scripts/01.Managers/DataManager.cs
+++ Assets/02.Scripts/01.Managers/DataManager.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 
+public class SaveData
+{
+    public readonly string saveFileHeader =  "UID,Name,Datas" ;
+    public readonly string saveFileHeaderType = "int,String,List<int>" ;
+
+    public int UID;
+    public string Name;
+    public List<int> Datas;
+
+    public SaveData(int id,string name,List<int> datas)
+    {
+        this.UID = id;
+        this.Name = name;
+        this.Datas = datas;
+    }
+}
+
 public class DataManager : SingletonDDOL<DataManager>
 {
     private readonly string csvfilePath = "CSV/TestData";
     private readonly string csvfileNumbering = "1";
 
+    private readonly string csvSaveFilePath = Application.dataPath + "/SaveData.csv";
+
     private StringBuilder strBuilder = new StringBuilder();
 
-    public CSVReader CsvReader = new CSVReader();
+    public CSVController CsvController = new CSVController();
 
     private List<Dictionary<string, object>> CsvData;
 
@@ -25,7 +44,7 @@ public class DataManager : SingletonDDOL<DataManager>
         strBuilder.Append(csvfilePath);
         strBuilder.Append(csvfileNumbering);
 
-        CsvData = CsvReader.Read(strBuilder.ToString());
+        CsvData = CsvController.Read(strBuilder.ToString());
 
         foreach (Dictionary<string, object> listdata  in CsvData)
         {
@@ -34,5 +53,18 @@ public class DataManager : SingletonDDOL<DataManager>
                 Debug.Log($"데이터 키 : {data.Key}  , 데이터 밸류 : {data.Value}");
             }
         }
+
+
+        //ToDoCode : 저장할데이터를 쓰는 코드 테스트 부분
+        List<int> datas = new List<int>();
+        datas.Add(2);
+        datas.Add(3);
+        datas.Add(5);
+
+        SaveData saveData = new SaveData(154351,"지존감자탕", datas);
+
+        strBuilder.Clear();
+        strBuilder.Append(csvSaveFilePath);
+        CsvController.Write(strBuilder.ToString(), saveData);
     }
 }
