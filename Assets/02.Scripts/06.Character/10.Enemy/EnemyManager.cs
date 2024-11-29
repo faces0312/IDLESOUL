1b5aa69e41802de9e0e33e5457b98c7d70203c30
HyunSeok Suh <suhya990312@gmail.com>
2024-11-29 15:25:33 +0900

    [Fix] EnemyManager Pool

--- Assets/02.Scripts/06.Character/10.Enemy/EnemyManager.cs
+++ Assets/02.Scripts/06.Character/10.Enemy/EnemyManager.cs
@@ -18,19 +18,26 @@ public class EnemyManager : MonoBehaviour
     }
     private void InitializeEnemyPool()
     {
-        ObjectPool goblinPool = new ObjectPool("Goblin", INITIAL_POOL_SIZE, "Prefabs/Enemy/Goblin");
+        ObjectPool goblinPool = new ObjectPool(5000, INITIAL_POOL_SIZE, "Prefabs/Enemy/Goblin");
+        /*ObjectPool goblinPriestPool = new ObjectPool("GoblinPriest", INITIAL_POOL_SIZE, "Prefabs/Enemy/GoblinPriest");
+        ObjectPool goblinKingPool = new ObjectPool("GoblinKing", INITIAL_POOL_SIZE, "Prefabs/Enemy/GoblinKing");
         ObjectPool orcPool = new ObjectPool("Orc", INITIAL_POOL_SIZE, "Prefabs/Enemy/Orc");
+        ObjectPool orcWarriorPool = new ObjectPool("OrcWarrior", INITIAL_POOL_SIZE, "Prefabs/Enemy/OrcWarrior");
+        ObjectPool orcGuardPool = new ObjectPool("OrcGuard", INITIAL_POOL_SIZE, "Prefabs/Enemy/OrcGuard");
+        ObjectPool batPool = new ObjectPool("Bat", INITIAL_POOL_SIZE, "Prefabs/Enemy/Bat");
+        ObjectPool spiderPool = new ObjectPool("Spider", INITIAL_POOL_SIZE, "Prefabs/Enemy/Spider");
+        ObjectPool snakePool = new ObjectPool("Snake", INITIAL_POOL_SIZE, "Prefabs/Enemy/Snake");*/
         ObjectPoolManager.Instance.AddPool(ENEMY_POOL_KEY, goblinPool);
-        ObjectPoolManager.Instance.AddPool(ENEMY_POOL_KEY, orcPool);
+        //ObjectPoolManager.Instance.AddPool(ENEMY_POOL_KEY, orcPool);
     }
     void SpawnCycle()
     {
-        EnemySpawn(1, "Goblin", 5000);
+        EnemySpawn(1, 5000);
     }
 
-    public void EnemySpawn(int cycle, string name, int id)
+    public void EnemySpawn(int cycle, int id)
     {
-        ObjectPool pool = ObjectPoolManager.Instance.GetPool(ENEMY_POOL_KEY, name);
+        ObjectPool pool = ObjectPoolManager.Instance.GetPool(ENEMY_POOL_KEY, id);
         for (int i =0; i<cycle; i++)
         {
             GameObject enemy = pool.GetObject();
