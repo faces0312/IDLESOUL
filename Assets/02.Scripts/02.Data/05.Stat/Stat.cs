f163ba18b41af643a587ee9675d1b135977260b7
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-11-29 19:43:09 +0900

    [Fix] StatHandler, Stat 수정
    StatHandler 연산 수정, Stat 연산자 오버로딩 추가

--- Assets/02.Scripts/02.Data/05.Stat/Stat.cs
+++ Assets/02.Scripts/02.Data/05.Stat/Stat.cs
@@ -1,4 +1,5 @@
 using ScottGarland;
+using System;
 
 public enum StatType
 {
@@ -30,7 +31,19 @@ public class Stat
 
     public Stat()
     {
+        this.health = 0;
+        this.maxHealth = 0;
+        this.atk = 0;
+        this.def = 0;
 
+        this.moveSpeed = 0;
+        this.atkSpeed = 0;
+
+        this.reduceDamage = 0;
+
+        this.critChance = 0;
+        this.critDamage = 0;
+        this.coolDown = 0;
     }
 
     public Stat(Stat stat)
@@ -48,5 +61,68 @@ public class Stat
         this.critChance = stat.critChance;
         this.critDamage = stat.critDamage;
         this.coolDown = stat.coolDown;
+    }
+
+    public static Stat operator +(Stat stat1, Stat stat2)
+    {
+        Stat clacStat = new Stat();
+
+        clacStat.health = stat1.health + stat2.health;
+        clacStat.maxHealth = stat1.maxHealth + stat2.maxHealth;
+        clacStat.atk = stat1.atk + stat2.atk;
+        clacStat.def = stat1.def + stat2.def;
+                
+        clacStat.moveSpeed = (float)Math.Round(stat1.moveSpeed + stat2.moveSpeed, 1);
+        clacStat.atkSpeed = (float)Math.Round(stat1.atkSpeed + stat2.atkSpeed, 1);
+                
+        clacStat.reduceDamage = (float)Math.Round(stat1.reduceDamage + stat2.reduceDamage, 1);
+                
+        clacStat.critChance = (float)Math.Round(stat1.critChance + stat2.critChance, 1);
+        clacStat.critDamage = (float)Math.Round(stat1.critDamage + stat2.critDamage, 1);
+        clacStat.coolDown = (float)Math.Round(stat1.coolDown + stat2.coolDown, 1);
+
+        return clacStat;
+    }
+
+    public static Stat operator -(Stat stat1, Stat stat2)
+    {
+        Stat clacStat = new Stat();
+
+        clacStat.health = stat1.health - stat2.health;
+        clacStat.maxHealth = stat1.maxHealth - stat2.maxHealth;
+        clacStat.atk = stat1.atk - stat2.atk;
+        clacStat.def = stat1.def - stat2.def;
+
+        clacStat.moveSpeed = (float)Math.Round(stat1.moveSpeed - stat2.moveSpeed, 1);
+        clacStat.atkSpeed = (float)Math.Round(stat1.atkSpeed - stat2.atkSpeed, 1);
+
+        clacStat.reduceDamage = (float)Math.Round(stat1.reduceDamage - stat2.reduceDamage, 1);
+
+        clacStat.critChance = (float)Math.Round(stat1.critChance - stat2.critChance, 1);
+        clacStat.critDamage = (float)Math.Round(stat1.critDamage - stat2.critDamage, 1);
+        clacStat.coolDown = (float)Math.Round(stat1.coolDown - stat2.coolDown, 1);
+
+        return clacStat;
+    }
+
+    public static Stat operator *(Stat stat, int amount)
+    {
+        Stat clacStat = new Stat();
+
+        clacStat.health = stat.health * amount;
+        clacStat.maxHealth = stat.maxHealth * amount;
+        clacStat.atk = stat.atk * amount;
+        clacStat.def = stat.def * amount;
+
+        clacStat.moveSpeed = (float)Math.Round(stat.moveSpeed * amount, 1);
+        clacStat.atkSpeed = (float)Math.Round(stat.atkSpeed * amount, 1);
+
+        clacStat.reduceDamage = (float)Math.Round(stat.reduceDamage * amount, 1);
+
+        clacStat.critChance = (float)Math.Round(stat.critChance * amount, 1);
+        clacStat.critDamage = (float)Math.Round(stat.critDamage * amount, 1);
+        clacStat.coolDown = (float)Math.Round(stat.coolDown * amount, 1);
+
+        return clacStat;
     }
 }
