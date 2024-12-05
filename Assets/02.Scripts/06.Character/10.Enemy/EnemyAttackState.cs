d7d96bf432fb3e35f08e03331b23a12685eccc1a
SONGMYEONGSEONG <100818983+SONGMYEONGSEONG@users.noreply.github.com>
2024-12-05 12:40:59 +0900

    [Update] EnemyProjectile 에 투사체에셋 적용

--- Assets/02.Scripts/06.Character/10.Enemy/EnemyAttackState.cs
+++ Assets/02.Scripts/06.Character/10.Enemy/EnemyAttackState.cs
@@ -63,14 +63,16 @@ public class EnemyAttackState : EnemyBaseState
         //Debug.Log("원거리공격");
         //원거리 적의 경우
         GameObject bulletInstance = 
-            Object.Instantiate(stateMachine.Enemy.bulletTest,new Vector3(stateMachine.Enemy.transform.position.x, stateMachine.Enemy.transform.position.y, stateMachine.Enemy.transform.position.z), Quaternion.Euler(Vector3.zero));
-        EnemyProjectile monsterBullet = bulletInstance.GetComponent<EnemyProjectile>();
-        monsterBullet.attack = stateMachine.Enemy.enemyDB.Attack;
-        monsterBullet.knockbackPower = stateMachine.Enemy.enemyDB.KnockBackPower;
+        Object.Instantiate(stateMachine.Enemy.bulletTest,new Vector3(stateMachine.Enemy.transform.position.x, stateMachine.Enemy.transform.position.y, stateMachine.Enemy.transform.position.z), Quaternion.Euler(Vector3.zero));
+        //EnemyProjectile monsterBullet = bulletInstance.GetComponent<EnemyProjectile>();
+        //monsterBullet.attack = stateMachine.Enemy.enemyDB.Attack;
+        //monsterBullet.knockbackPower = stateMachine.Enemy.enemyDB.KnockBackPower;
+
 
         Vector3 playerProjection = new Vector3(stateMachine.Enemy.target.transform.position.x, stateMachine.Enemy.target.transform.position.y, stateMachine.Enemy.target.transform.position.z);
         Vector3 selfProjection = new Vector3(stateMachine.Enemy. transform.position.x, stateMachine.Enemy.transform.position.y, stateMachine.Enemy.transform.position.z);
 
-        monsterBullet.dir = (playerProjection - selfProjection).normalized;
+        bulletInstance.GetComponent<EnemyProjectile>().dir = (playerProjection - selfProjection).normalized;
+        //monsterBullet.dir = (playerProjection - selfProjection).normalized;
     }
 }
