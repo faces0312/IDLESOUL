c8601e468023150f62b626d2db006d2c02bc1828
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-12-05 17:02:52 +0900

    [Update] SlashDance 구현
    SlashDance 구현

---
+++ Assets/02.Scripts/08.Skill/01.Knight/SlashDanceHit.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScottGarland;
+
+public class SlashDanceHit : MonoBehaviour
+{
+    [SerializeField] private float tickTime = 0.05f;
+    private float lifeTime;
+
+    private float curTime;
+
+    private BigInteger value;
+    private float time;
+
+    private Collider myCollider;
+    private LayerMask layerMask;
+
+    private Coroutine curCorutine;
+    private WaitForSecondsRealtime coroutineTime;
+
+    void Start()
+    {
+        lifeTime = time - 1f;
+        curTime = Time.time;
+        myCollider = GetComponent<Collider>();
+        layerMask = 1 << LayerMask.NameToLayer("Enemy");
+        coroutineTime = new WaitForSecondsRealtime(tickTime);
+    }
+
+    void Update()
+    {
+        // 지속 시간 후 삭제
+        if (Time.time > curTime + lifeTime)
+        {
+            if (curCorutine != null)
+                StopCoroutine(curCorutine);
+                Destroy(gameObject);
+        }
+    }
+
+    public void InitSettings(BigInteger value, float time)
+    {
+        this.value = value;
+        this.time = time;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Utils.IsInLayerMask(other.gameObject.layer, layerMask))
+        {
+            if (curCorutine == null)
+                curCorutine = StartCoroutine(CoroutineTickDamage());
+        }
+    }
+
+    private IEnumerator CoroutineTickDamage()
+    {
+        while (true)
+        {
+            // TODO : Enemy 피격 처리
+
+            Debug.LogAssertion("Slash Damage!");
+            yield return coroutineTime;
+        }
+    }
+}
