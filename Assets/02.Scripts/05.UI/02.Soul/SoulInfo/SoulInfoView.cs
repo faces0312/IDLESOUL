cdbbe567827d49f2d3d8f87f02722ddf3af20cda
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-12-11 11:29:08 +0900

    [Update] SoulInfo Upgrade 연결
    Upgrade 부분 연결

--- Assets/02.Scripts/05.UI/02.Soul/SoulInfo/SoulInfoView.cs
+++ Assets/02.Scripts/05.UI/02.Soul/SoulInfo/SoulInfoView.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum LevelType
 {
-    Soul,
     Default,
     Ultimate,
-    Passive
+    Passive,
+    Soul
 }
 
 public class SoulInfoView : MonoBehaviour, IUIBase
@@ -21,13 +22,22 @@ public class SoulInfoView : MonoBehaviour, IUIBase
     [SerializeField] private GameObject passiveSkill;
 
     [Header("Level")]
-    [SerializeField] private TextMeshProUGUI[] levelText;
+    [SerializeField] private TextMeshProUGUI[] levelTexts;
+
+    [Header("Icon")]
+    [SerializeField] private Image[] icons;
+
+    [Header("Name")]
+    [SerializeField] private TextMeshProUGUI[] skillNameTexts;
+
+    [Header("Description")]
+    [SerializeField] private TextMeshProUGUI[] skillDescriptionTexts;
 
     public Soul soul;
 
     public void Initialize()
     {
-
+        
     }
 
     public void ShowUI()
@@ -50,30 +60,52 @@ public class SoulInfoView : MonoBehaviour, IUIBase
     public void UpdateUI()
     {
         Debug.LogAssertion("소울 인포 UI 업데이트");
-        levelText[(int)LevelType.Soul].text = $"Lv. {soul.level}";
+        levelTexts[(int)LevelType.Soul].text = $"Lv. {soul.level}";
     }
 
     public void UpdateDefault()
     {
         Debug.LogAssertion("소울 인포 스킬1 업데이트");
-        levelText[(int)LevelType.Default].text = $"Lv. {soul.Skills[(int)SkillType.Default].level}";
+        levelTexts[(int)LevelType.Default].text = $"Lv. {soul.Skills[(int)SkillType.Default].level}";
     }
 
     public void UpdateUltimate()
     {
         Debug.LogAssertion("소울 인포 스킬2 업데이트");
-        levelText[(int)LevelType.Ultimate].text = $"Lv. {soul.Skills[(int)SkillType.Ultimate].level}";
+        levelTexts[(int)LevelType.Ultimate].text = $"Lv. {soul.Skills[(int)SkillType.Ultimate].level}";
     }
 
     public void UpdatePassive()
     {
         Debug.LogAssertion("소울 인포 패시브 업데이트");
-        levelText[(int)LevelType.Passive].text = $"Lv. {soul.Skills[(int)SkillType.Passive].level}";
+        levelTexts[(int)LevelType.Passive].text = $"Lv. {soul.Skills[(int)SkillType.Passive].level}";
     }
 
     private void ConnectSoul()
     {
         SoulInfoController controller = UIManager.Instance.GetController("SoulInfo") as SoulInfoController;
-        soul = controller.SoulInfoModel.soul;
+
+        if(controller.SoulInfoModel.soul != soul)
+        {
+            soul = controller.SoulInfoModel.soul;
+
+            InitUI();
+        }
+    }
+
+    private void InitUI()
+    {
+        // TODO : 스킬 sprite 삽입
+        // icons[(int)LevelType.Default].sprite = 
+        // icons[(int)LevelType.Ultimate].sprite = 
+        // icons[(int)LevelType.Passive].sprite = 
+
+        skillNameTexts[(int)LevelType.Default].text = $"Lv. {soul.Skills[(int)SkillType.Default].skillName}";
+        skillNameTexts[(int)LevelType.Ultimate].text = $"Lv. {soul.Skills[(int)SkillType.Ultimate].skillName}";
+        skillNameTexts[(int)LevelType.Passive].text = $"Lv. {soul.Skills[(int)SkillType.Passive].skillName}";
+
+        skillDescriptionTexts[(int)LevelType.Default].text = $"Lv. {soul.Skills[(int)SkillType.Default].description}";
+        skillDescriptionTexts[(int)LevelType.Ultimate].text = $"Lv. {soul.Skills[(int)SkillType.Ultimate].description}";
+        skillDescriptionTexts[(int)LevelType.Passive].text = $"Lv. {soul.Skills[(int)SkillType.Passive].description}";
     }
 }
