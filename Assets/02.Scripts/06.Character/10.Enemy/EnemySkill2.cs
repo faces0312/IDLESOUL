1b3d37f2df2472ee70f354372356e8ef5955a492
HyunSeok Suh <suhya990312@gmail.com>
2024-12-13 20:14:10 +0900

    [Update] Skeleton Boss Enemy 완성

--- Assets/02.Scripts/06.Character/10.Enemy/EnemySkill2.cs
+++ Assets/02.Scripts/06.Character/10.Enemy/EnemySkill2.cs
@@ -25,40 +25,7 @@ public class EnemySkill2 : EnemySkillBase
 
     public void SkillAttack2()
     {
-        bulletInstances = EnemyManager.Instance.EnemyAttackSpawn(6007, new Vector3(GameManager.Instance.player.transform.position.x, GameManager.Instance.player.transform.position.y + 8f, GameManager.Instance.player.transform.position.z), Quaternion.Euler(90, 0, 0));
+        bulletInstances = EnemyManager.Instance.EnemyAttackSpawn(6007, new Vector3(bossEnemy.skillZone.transform.GetChild(0).position.x, bossEnemy.skillZone.transform.GetChild(0).position.y + 8f, bossEnemy.skillZone.transform.GetChild(0).position.z), Quaternion.Euler(90, 0, 0));
 
-        EnemyProjectile projectile = bulletInstances.GetComponent<EnemyProjectile>();
-        //skill 데미지로 설정
-        projectile.attack = stateMachine.Enemy.enemyDB.Attack;
-        projectile.knockbackPower = stateMachine.Enemy.enemyDB.KnockBackPower;
-
-        if (projectile != null)
-        {
-            projectile.dir = Vector3.down;//rotation.eulerAngles;
-        }
-        /*int bulletCount = 8;
-        float angleStep = 45f; // 45도 간격
-        GameObject[] bulletInstances = new GameObject[bulletCount];
-
-        for (int i = 0; i < bulletCount; i++)
-        {
-            float angle = i * angleStep; // 0도부터 시작하여 45도씩 증가
-            Quaternion rotation = Quaternion.Euler(0, angle, 0);
-
-            bulletInstances[i] = EnemyManager.Instance.EnemyAttackSpawn(6003, new Vector3(stateMachine.Enemy.transform.position.x, stateMachine.Enemy.transform.position.y, stateMachine.Enemy.transform.position.z), rotation);
-
-            EnemyProjectile projectile = bulletInstances[i].GetComponent<EnemyProjectile>();
-            //skill 데미지로 설정
-            projectile.attack = stateMachine.Enemy.enemyDB.Attack;
-            projectile.knockbackPower = stateMachine.Enemy.enemyDB.KnockBackPower;
-
-            if (projectile != null)
-            {
-                projectile.dir = rotation * Vector3.right;//rotation.eulerAngles;
-            }
-            *//*// 각 총알의 방향을 계산
-            Vector3 direction = Quaternion.Euler(0, 0, angle) * Vector3.right;
-            monsterBullet.Initialize(direction);*//*
-        }*/
     }
 }
