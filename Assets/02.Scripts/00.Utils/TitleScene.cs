1e145011898b380d6a183be353fec08c927df7e5
zkco <qkrentks456@gmail.com>
2024-12-17 19:40:44 +0900

    [Add] 타이틀 씬 터치 시 게임 씬 진입

---
+++ Assets/02.Scripts/00.Utils/TitleScene.cs
@@ -0,0 +1,11 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TitleScene : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneDataManager.Instance.LoadScene("GameScene");
+    }
+}
