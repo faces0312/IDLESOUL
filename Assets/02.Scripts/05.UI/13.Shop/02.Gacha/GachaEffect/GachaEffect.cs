6c8bbf7c613f6fc6a3671df9e71f73caf6d24dd2
zkco <qkrentks456@gmail.com>
2025-01-07 15:38:11 +0900

    [유저 피드백] 가챠 연출 중 게임 일시정지

--- Assets/02.Scripts/05.UI/13.Shop/02.Gacha/GachaEffect/GachaEffect.cs
+++ Assets/02.Scripts/05.UI/13.Shop/02.Gacha/GachaEffect/GachaEffect.cs
@@ -16,11 +16,15 @@ public class GachaEffect : MonoBehaviour
     {
         EventManager.Instance.OnPickUpSoul += ShowEffect;
         this.gameObject.SetActive(false);
+        DOTween.Init();
+        DOTween.useSafeMode = true;
     }
 
     private void MakeSquence()
     {
+        Time.timeScale = 0;
         Sequence seq = DOTween.Sequence();
+        seq.SetUpdate(true);
         seq.Append(gachaImage.transform.DOMoveY(600, 0));
         seq.Append(gachaText.DOFade(0, 0));
         seq.Append(gachaText.DOFade(1, 1));
@@ -30,6 +34,7 @@ public class GachaEffect : MonoBehaviour
         seq.Append(gachaImage.DOColor(Color.black, 1));
         seq.OnComplete(() =>
         {
+            Time.timeScale = 1;
             this.gameObject.SetActive(false);
         });
     }
