27b28b3adb67eeef942a1e6041fa598b3432683d
zkco <qkrentks456@gmail.com>
2025-01-07 21:33:51 +0900

    [Update] 1차 다이얼로그 작성

--- Assets/02.Scripts/05.UI/21.UITalk/ChatUICanvas.cs
+++ Assets/02.Scripts/05.UI/21.UITalk/ChatUICanvas.cs
@@ -1,51 +1,14 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ChatUICanvas : MonoBehaviour, IPointerClickHandler
+public class ChatUICanvas : MonoBehaviour
 {
     [SerializeField] private Image CharacterImage;
     [SerializeField] private Text CharacterName;
     [SerializeField] private Text CharacterText;
-    [SerializeField] private Button Skip;
 
-    private bool isPlay;
-    private bool isConfirm;
-    private bool isSkip;
-
-    private WaitUntil click;
-
-    private void Start()
+    public void Print()
     {
-        click = new WaitUntil(() =>
-        {
-            if (isSkip == true) return true;
-            return isConfirm;
-        });
-    }
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        isConfirm = true;
-    }
-
-    public void PrintDialogue(int cycle, int index)
-    {
-        var audioSource = ObjectPoolManager.Instance.GetPool(Const.AUDIO_SOURCE_KEY, Const.AUDIO_SOURCE_POOL_KEY).GetObject();
-        audioSource.SetActive(true);
-        AudioSource audio = audioSource.GetComponent<AudioSource>();
-        audio.Play();
-    }
-
-    private IEnumerator StartTalk()
-    {
-        while(isPlay == true)
-        {
-
-            isConfirm = false;
-            yield return click;
-            yield return null;
-        }
     }
 }
